d3bf1010ce1e0610d628fdbe70c3745bf83046a2
“ofugeez” <eofugara@gamil.com>
2022-09-08 14:56:29 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,28 @@
-#!/bin/bash
-#include <stdio.h>
-/**
- * main - print the put function
- *
- * Return: 0 after printing the function
- */
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}
+#!/bin/bash                                                                                                                                   
+
+#include <stdio.h>  
+
+/**                                                                                                                                           
+
+ * main - print the string in the put function                                                                                                              
+
+ *                                                                                                                                            
+
+ * Description: using the main function 
+
+* this program prints “Programming is like building a multilingual puzzle
+
+* Return: 0 after printing the function                                                                                                       
+
+ */                                                                                                                                           
+
+int main(void)                                                                                                                                
+
+{
+
+puts("\"Programming is like building a multilingual puzzle"); 
+
+       	 return (0);                                                                                                                           
+
+}       
+
