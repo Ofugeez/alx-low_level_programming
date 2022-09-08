b104fc2cd54d431c34f6d40b7d52ce11b09d0e06
“ofugeez” <eofugara@gamil.com>
2022-09-08 16:46:59 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - print the sting in the put function
- *
- * Description: use the main funtion
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
