1df9216fb15ca85f421cda7d8691c3fa488fbe66
“ofugeez” <eofugara@gamil.com>
2022-09-16 02:50:41 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers 0 to 9
- * followed by a new line
+ * print_numbers - function that print all numbers from
+ * 0 to 9
  *
  * Return: Always 0
  */
+
 void print_numbers(void)
 {
-	int i;
-	
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
+	int n;
 
-	_putchar('\n');
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
 }
