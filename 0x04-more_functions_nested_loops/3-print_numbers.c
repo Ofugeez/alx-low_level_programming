708880b90229a4a6490f3a3c03e84c105b6884a1
“ofugeez” <eofugara@gamil.com>
2022-09-16 01:40:28 -0700

    3-print_numbers.c fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
+ * print_numbers - prints the numbers 0 to 9
  * followed by a new line
  *
- * Return: Alway 0
+ * Return: Always 0
  */
 void print_numbers(void)
 {
