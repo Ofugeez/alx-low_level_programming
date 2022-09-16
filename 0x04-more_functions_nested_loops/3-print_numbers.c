06f5da4fe2a0f2a9e98fa945313fd570f53863a8
“ofugeez” <eofugara@gamil.com>
2022-09-15 21:27:28 -0700

    3-print_numbers.c fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,12 @@
 /**
  * print_numbers - checks for digits 0 through 9
  * followed by a new line
- *
  * Return: Always 0
  */
 void print_numbers(void)
 {
 	int i:
-		for (i = 48; i <= 58; i++)
+		for (i = 48; i < 58; i++)
 		{
 			_putchar(i);
 		}
