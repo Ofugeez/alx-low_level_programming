90a9357372bd0d4f3b5f26a8ef6e2827b1ef1538
“ofugeez” <eofugara@gamil.com>
2022-09-26 13:51:26 -0700

    5-strstr.c fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,15 +25,15 @@ char *_strstr(char *haystack, char *needle)
 			do {
 				if (needle[index + 1] == '\0')
 					return (haystack);
-				
+
 				index++;
 
 			} while (haystack[index] == needle[index]);
 		}
-		
+
 		haystack++;
 	}
-	
+
 	return ('\0');
 }
 
