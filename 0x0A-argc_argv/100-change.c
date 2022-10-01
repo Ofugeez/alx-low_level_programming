4f872590ca7af8e7be5848f4fc8b8a9b1e107118
“ofugeez” <eofugara@gamil.com>
2022-10-01 09:01:34 -0700

    Space fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,13 +23,13 @@ int main(int argc, char *argv[])
 
 	num = atoi(argv[1]);
 	result = 0;
-	
+
 	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	
+
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 			num -= coins[j];
 		}
 	}
-	
+
 	printf("%d\n", result);
 	return (0);
 }
