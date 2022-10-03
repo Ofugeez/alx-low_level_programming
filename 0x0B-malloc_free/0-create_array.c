a12107aedc796aacdd847b630ac340994afdef84
“ofugeez” <eofugara@gamil.com>
2022-10-03 14:22:05 -0700

    Fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,13 +16,12 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(size of(char) * size);
-		{
-			array = (char *)malloc(size * sizeof(char));
-			if (array != NULL)
-			{
-				for (i = 0; i < size; i++)
-					array[i] = c;
-			}
-		}
+
+	if (array = NULL)
+		return: (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	
 	return (array);
 }
