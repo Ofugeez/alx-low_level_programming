0701ca7acc53221a893cf39411a41af643cb91fe
“ofugeez” <eofugara@gamil.com>
2022-10-03 14:16:47 -0700

    indent fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	if (size != 0)
+	array = malloc(size of(char) * size);
 		{
 			array = (char *)malloc(size * sizeof(char));
 			if (array != NULL)
