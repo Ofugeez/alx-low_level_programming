797f737f9b3b7b15d7ec99740912492c297be706
“ofugeez” <eofugara@gamil.com>
2022-10-03 16:45:52 -0700

    SPacing

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _isspace - check if a character is whitespace
+ * @c: the character to check
+ *
+ * Return: 1 is c is a whitespace character, otherwise 0
+ */
+int _isspace(int c)
+{
+	if (c == 0x20 || (c >= 0x09 && c <= 0x0d))
+		return (1);
+	return (0);
+}
+
+
+/**
  * strtow - split a string into words
  * @str: a pointer to the string to split
  *
