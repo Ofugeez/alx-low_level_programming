445c0125ec84afe266aaa18784931ebdebc2a4be
“ofugeez” <eofugara@gamil.com>
2022-10-03 16:37:46 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,42 +1,52 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * **strtow - function that splits a string into words.
- * @str : pointer
- * Return: Char
+ * strtow - split a string into words
+ * @str: a pointer to the string to split
+ *
+ * Return: NULL if memory allocation fails or if str is NULL or empty (""),
+ * otherwise return a pointer to the array of words terminated by a NULL
  */
-
 char **strtow(char *str)
 {
-	char **d;
-	int i;
-	int j = 0;
-	int con = 0;
+	char **words, *pos = str;
+	int w = 0, c;
 
-	if (str == NULL)
+	if (!(str && *str))
 		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
-			con++;
-	}
-
-	d = malloc(sizeof(char) * con);
-	if (d == NULL)
+	do {
+		while (_isspace(*pos))
+			++pos;
+		if (!*pos)
+			break;
+		while (*(++pos) && !_isspace(*pos))
+			;
+	} while (++w, *pos);
+	if (!w)
 		return (NULL);
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-
-		if (str[i] != 32)
+	words = (char **) malloc(sizeof(char *) * (w + 1));
+	if (!words)
+		return (NULL);
+	w = 0, pos = str;
+	do {
+		while (_isspace(*pos))
+			++pos;
+		if (!*pos)
+			break;
+		or (str = pos++; *pos && !_isspace(*pos); ++pos)
+			;
+		words[w] = (char *) malloc(sizeof(char) * (pos - str + 1));
+		if (!words[w])
 		{
-			*d[j] = str[i];
-			j++;
+			while (w >  0)
+				free(words[--w]);
+			free(words);
+			return (NULL);
 		}
-		else
-		{
-		}
-	}
-	return (d);
+		for (c = 0; str < pos; ++c, ++str)
+			words[w][c] = *str;
+		words[w][c] = '\0';
+	} while (++w, *pos);
+	words[w] = NULL;
+	return (words);
 }
