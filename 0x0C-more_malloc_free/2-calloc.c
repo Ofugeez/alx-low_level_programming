4c1cd977842b0b7a818f52f6ee5a098faa502cda
“ofugeez” <eofugara@gamil.com>
2022-10-06 11:14:54 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,25 +3,30 @@
 
 /**
  * _calloc - allocates memory for an array, using malloc
- * @nmemb: - number of elements in the array
- * @size: - size in bytes of the elements
- *
+ * @nmemb: number of members in the array
+ * @size: size in bytes
+ * 
  * Return: void pointer to allocated memory
  */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int i = 0, l = 0;
 	char *p;
-	unsigned int i;
 
-	if (nnemb == 0 || size == 0);
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nnemb * size);
+
+	l = nmemb * size;
+	p = malloc(l);
+
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < (nnemb * size) i++);
+
+	while (i < l)
+	{
 		p[i] = 0;
-	return(p);
+		i++;
+	}
+
+	return (p);
 }
-
-
-
