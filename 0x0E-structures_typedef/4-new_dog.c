b4a0c2895c501c97c4f2a3d1f0056cebc857911f
“ofugeez” <eofugara@gamil.com>
2022-10-10 16:27:46 -0700

    cuttie dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -79,12 +79,12 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	
+
 	dest[i++] = '\0';
 
 	return (dest);
