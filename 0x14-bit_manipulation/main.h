d994ceb8c19e4f53b17b0f021a424ad263f29baf
“ofugeez” <eofugara@gamil.com>
2022-10-27 20:25:47 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -12,4 +9,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
-#endif
+#endif /* _MAIN_H_ */
